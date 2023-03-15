991959c5509fa49f6c1f794b653f3d3c8795d630
mikkelwoelk <mikkelwolk@gmail.com>
2023-03-15 14:10:16 +0100

    feat(index): added shine effect to links

--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="md:px-10 px-5 py-10 md:py-16">
     <h1 class="text-center text-6xl md:text-8xl mb-10 md:mb-20" v-text="pageTitle" />
-    <div class="grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 max-w-7xl mx-auto">
-      <div v-for="page in pages">
+    <div
+      class="c-page-list grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 max-w-7xl mx-auto"
+    >
+      <div class="c-page-list__item relative overflow-hidden shadow-2xl rounded-xl" v-for="page in pages">
         <NuxtLink :to="page.url">
-          <CardContainer :card-content="pages[0]"> </CardContainer>
+          <CardContainer :card-content="pages[0]"></CardContainer>
         </NuxtLink>
       </div>
     </div>
@@ -18,4 +20,35 @@ const pages = ref([
 ]);
 </script>
 
-<style lang="scss" scoped></style>
+<style>
+@keyframes shine {
+  0% {
+    left: -100px;
+    opacity: 0;
+  }
+  20% {
+    opacity: 0.8;
+  }
+  100% {
+    left: 300%;
+    opacity: 0;
+  }
+}
+
+.c-page-list__item:hover:before {
+  content: '';
+  position: absolute;
+  width: 100px;
+  height: 100%;
+  opacity: 0.5;
+  background-image: linear-gradient(
+    120deg,
+    rgba(255, 255, 255, 0) 30%,
+    rgba(255, 255, 255, 0.8),
+    rgba(255, 255, 255, 0) 70%
+  );
+  top: 0;
+  left: -100px;
+  animation: shine 1500ms forwards;
+}
+</style>
