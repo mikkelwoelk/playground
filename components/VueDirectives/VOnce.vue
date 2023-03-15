7148dc5551bf869357ec66dfe717675bc3929d5b
mikkelwoelk <mikkelwolk@gmail.com>
2023-03-15 16:07:40 +0100

    feat(VOnce): initialized component

---
+++ components/VueDirectives/VOnce.vue
@@ -0,0 +1,14 @@
+<template>
+  <CardContainer :card-content="cardContent">
+    <template #content> </template>
+  </CardContainer>
+</template>
+
+<script setup>
+const cardContent = ref({
+  title: 'V-Once',
+  info: 'Render the element and component once only, and skip future updates.',
+});
+</script>
+
+<style></style>
