7b6f122311160c4a0cc047617f30d2622546b988
mikkelwoelk <mikkelwolk@gmail.com>
2023-03-15 16:07:46 +0100

    feat(VPre): initialized component

---
+++ components/VueDirectives/VPre.vue
@@ -0,0 +1,14 @@
+<template>
+  <CardContainer :card-content="cardContent">
+    <template #content> </template>
+  </CardContainer>
+</template>
+
+<script setup>
+const cardContent = ref({
+  title: 'V-Pre',
+  info: 'Skip compilation for this element and all its children.',
+});
+</script>
+
+<style></style>
