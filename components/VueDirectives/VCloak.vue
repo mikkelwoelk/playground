9a44a84b9a62d248dbb4546c9cd6b77d092d0df8
mikkelwoelk <mikkelwolk@gmail.com>
2023-03-15 16:07:22 +0100

    feat(VCloak): initialized component

---
+++ components/VueDirectives/VCloak.vue
@@ -0,0 +1,14 @@
+<template>
+  <CardContainer :card-content="cardContent">
+    <template #content> </template>
+  </CardContainer>
+</template>
+
+<script setup>
+const cardContent = ref({
+  title: 'V-Cloak',
+  info: 'Used to hide un-compiled template until it is ready.',
+});
+</script>
+
+<style></style>
