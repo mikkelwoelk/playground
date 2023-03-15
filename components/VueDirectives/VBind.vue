77654cca09423a4d0b66eb9e00ba9eba3baf02d0
mikkelwoelk <mikkelwolk@gmail.com>
2023-03-15 16:06:54 +0100

    feat(Bind): added interactive

--- components/VueDirectives/VBind.vue
+++ components/VueDirectives/VBind.vue
@@ -1,12 +1,39 @@
 <template>
-  <CardContainer :card-content="cardContent">
+  <CardContainer
+    :card-content="cardContent"
+    :class="{
+      'bg-red-500': selectedOption == 'red',
+      'bg-green-500': selectedOption == 'green',
+      'bg-blue-500': selectedOption == 'blue',
+    }"
+    class="transition-all duration-300 ease-in-out"
+  >
     <template #content>
-      <select name="colors" id="colors" class="w-fit">
-        <option value="default" selected disabled>Select a color to bind</option>
-        <option value="red">Red</option>
-        <option value="blue">Blue</option>
-        <option value="green">Green</option>
-      </select>
+      <div class="flex flex-col items-center">
+        <div class="flex flex-col items-center">
+          <span class="text-sm mb-5">Press a button to bind a color to this card.</span>
+        </div>
+        <div class="w-full flex justify-around">
+          <button
+            class="border-2 border-white px-2 py-1 rounded-lg hover:bg-red-500 transition-all duration-300 ease-in-out"
+            @click="() => changeColor('red')"
+          >
+            Red
+          </button>
+          <button
+            class="border-2 border-white px-2 py-1 rounded-lg hover:bg-green-500 transition-all duration-300 ease-in-out"
+            @click="() => changeColor('green')"
+          >
+            Green
+          </button>
+          <button
+            class="border-2 border-white px-2 py-1 rounded-lg hover:bg-blue-500 transition-all duration-300 ease-in-out"
+            @click="() => changeColor('blue')"
+          >
+            Blue
+          </button>
+        </div>
+      </div>
     </template>
   </CardContainer>
 </template>
@@ -21,6 +48,23 @@ const cardContent = ref({
     { title: '.attr', info: 'Force a binding to be set as a DOM attribute. ' },
   ],
 });
+
+const selectedOption = ref('');
+
+const changeColor = (color) => {
+  if (color == 'red') {
+    selectedOption.value = 'red';
+  }
+  if (color == 'green') {
+    selectedOption.value = 'green';
+  }
+  if (color == 'blue') {
+    selectedOption.value = 'blue';
+  }
+  setTimeout(() => {
+    selectedOption.value = '';
+  }, '3000');
+};
 </script>
 
 <style scoped></style>
