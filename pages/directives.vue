5ecbfa7fc068dd8f480309048d68f11f3ebcc6df
mikkelwoelk <mikkelwolk@gmail.com>
2023-03-15 14:09:47 +0100

    chore(directives): added component class name

--- pages/directives.vue
+++ pages/directives.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="md:px-10 px-5 py-10 md:py-16 relative">
+  <div class="c-directives md:px-10 px-5 py-10 md:py-16 relative">
     <BackButton />
     <h1 class="text-center text-6xl md:text-8xl mb-10 md:mb-20" v-text="pageTitle" />
-    <div class="grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 max-w-7xl mx-auto">
+    <div class="c-directives-list grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 max-w-7xl mx-auto">
       <VueDirectivesVModel />
       <VueDirectivesVIf />
       <VueDirectivesVElse />
@@ -18,4 +18,4 @@
 const pageTitle = ref('Directives');
 </script>
 
-<style lang="scss" scoped></style>
+<style></style>
