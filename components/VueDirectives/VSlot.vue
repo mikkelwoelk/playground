4aecd37b495dd013e5b7bb2c9cc11d15b60c9d66
mikkelwoelk <mikkelwolk@gmail.com>
2023-03-15 16:07:54 +0100

    feat(VSlot): initialized component

---
+++ components/VueDirectives/VSlot.vue
@@ -0,0 +1,14 @@
+<template>
+  <CardContainer :card-content="cardContent">
+    <template #content> </template>
+  </CardContainer>
+</template>
+
+<script setup>
+const cardContent = ref({
+  title: 'V-Slot',
+  info: 'Denote named slots or scoped slots that expect to receive props. Has a shorthand: #',
+});
+</script>
+
+<style></style>
