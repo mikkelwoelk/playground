a722633c9dcfc34fa2fa3f765f94b004daefa12e
mikkelwoelk <mikkelwolk@gmail.com>
2023-03-17 11:36:40 +0100

    feat(VPre): added directive example

--- components/VueDirectives/VPre.vue
+++ components/VueDirectives/VPre.vue
@@ -1,6 +1,9 @@
 <template>
   <CardContainer :card-content="cardContent">
-    <template #content> </template>
+    <template #content>
+      <span> Without v-pre: {{ message }}</span>
+      <span v-pre> With v-pre: {{ message }}</span>
+    </template>
   </CardContainer>
 </template>
 
@@ -9,6 +12,8 @@ const cardContent = ref({
   title: 'V-Pre',
   info: 'Skip compilation for this element and all its children.',
 });
+
+const message = ref('Just showing off the v-pre directive');
 </script>
 
 <style></style>
