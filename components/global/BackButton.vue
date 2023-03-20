3d8edc5bebf4d40471b2b2516c0568149734bccb
mikkelwoelk <mikkelwolk@gmail.com>
2023-03-20 09:12:09 +0100

    fix(BackButton): corrected path to svg

--- components/global/BackButton.vue
+++ components/global/BackButton.vue
@@ -3,7 +3,7 @@
     <NuxtLink :to="'/'">
       <img
         class="w-10 fill-white hover:-translate-x-1 transition-all duration-200 ease-in-out"
-        src="~~/src/img/arrow-left-icon.svg"
+        src="/arrow-left-icon.svg"
         alt=""
       />
     </NuxtLink>
