114b6da7b2d997364a78c14953722aa0b7b4c066
mikkelwoelk <mikkelwolk@gmail.com>
2023-03-20 09:47:00 +0100

    fix(CardContainer): added `h-full`

--- components/global/CardContainer.vue
+++ components/global/CardContainer.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     v-if="cardContent"
-    class="c-card-container relative flex flex-col justify-between py-5 px-8 w-full shadow-2xl rounded-xl"
+    class="c-card-container relative flex flex-col h-full justify-between py-5 px-8 w-full shadow-2xl rounded-xl"
   >
     <Transition name="t-fade-in">
       <ModifierOverlay v-if="show" :card-content="cardContent" />
