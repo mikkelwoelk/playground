42ebc026546a45118e97819207f515766480d6f7
mikkelwoelk <mikkelwolk@gmail.com>
2023-04-03 10:56:41 +0200

    fix(index & directives): added wrapper to keep root element classes clean

--- pages/directives.vue
+++ pages/directives.vue
@@ -1,20 +1,24 @@
 <template>
-  <div class="c-directives md:px-10 px-5 py-10 md:py-16 relative">
-    <BackButton />
-    <h1 class="text-center text-6xl md:text-8xl mb-10 md:mb-20" v-text="pageTitle" />
-    <div class="c-directives-list grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 max-w-7xl mx-auto">
-      <VueDirectivesVModel />
-      <VueDirectivesVIf />
-      <VueDirectivesVElse />
-      <VueDirectivesVElseIf />
-      <VueDirectivesVFor />
-      <VueDirectivesVOn />
-      <VueDirectivesVBind />
-      <VueDirectivesVSlot />
-      <VueDirectivesVPre />
-      <VueDirectivesVOnce />
-      <VueDirectivesVMemo />
-      <VueDirectivesVCloak />
+  <div class="p-directives">
+    <div class="md:px-10 px-5 py-10 md:py-16 relative">
+      <BackButton />
+      <h1 class="text-center text-6xl md:text-8xl mb-10 md:mb-20" v-text="pageTitle" />
+      <div
+        class="c-directives-list grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 max-w-7xl mx-auto"
+      >
+        <VueDirectivesVModel />
+        <VueDirectivesVIf />
+        <VueDirectivesVElse />
+        <VueDirectivesVElseIf />
+        <VueDirectivesVFor />
+        <VueDirectivesVOn />
+        <VueDirectivesVBind />
+        <VueDirectivesVSlot />
+        <VueDirectivesVPre />
+        <VueDirectivesVOnce />
+        <VueDirectivesVMemo />
+        <VueDirectivesVCloak />
+      </div>
     </div>
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="md:px-10 px-5 py-10 md:py-16">
-    <h1 class="text-center text-6xl md:text-8xl mb-10 md:mb-20" v-text="pageTitle" />
-    <div
-      class="c-page-list grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 max-w-7xl mx-auto"
-    >
-      <div class="c-page-list__item relative overflow-hidden shadow-2xl rounded-xl" v-for="(page, index) in pages">
-        <NuxtLink :to="page.url">
-          <CardContainer :card-content="pages[index]"></CardContainer>
-        </NuxtLink>
+  <div class="p-frontpage">
+    <div class="md:px-10 px-5 py-10 md:py-16">
+      <h1 class="text-center text-6xl md:text-8xl mb-10 md:mb-20" v-text="pageTitle" />
+      <div
+        class="c-page-list grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 max-w-7xl mx-auto"
+      >
+        <div class="c-page-list__item relative overflow-hidden shadow-2xl rounded-xl" v-for="(page, index) in pages">
+          <NuxtLink :to="page.url">
+            <CardContainer :card-content="pages[index]"></CardContainer>
+          </NuxtLink>
+        </div>
       </div>
     </div>
   </div>
