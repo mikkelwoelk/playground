f322e93829fb5a5cb85cf9bef512840b7a371d18
mikkelwoelk <mikkelwolk@gmail.com>
2023-04-03 10:57:29 +0200

    feat(todo): added more features

--- pages/todo.vue
+++ pages/todo.vue
@@ -1,17 +1,27 @@
 <template>
-  <div class="c-directives md:px-10 px-5 py-10 md:py-16 relative">
-    <BackButton />
-    <h1 class="text-center text-6xl md:text-8xl mb-10 md:mb-20" v-text="pageTitle" />
-    <div>
-      <input v-model="newTodo.title" type="text" ref="input" @keyup.enter="() => addTodo()" />
-      <button class="py-1 px-2 border-2" @click="() => addTodo()">Add</button>
-      <ul>
-        <li v-for="todo in todoList" :key="`todo-id-${todo.id}`">
-          <!-- <input type="checkbox" /> -->
-          {{ todo.title }}
-          <!-- <button class="py-1 px-2 bg-red-500" @click="() => deleteTodo(todo.id)">DELETE</button> -->
-        </li>
-      </ul>
+  <div class="p-todo">
+    <div class="md:px-10 px-5 py-10 md:py-16 relative">
+      <BackButton />
+      <h1 class="text-center text-6xl md:text-8xl mb-10 md:mb-20" v-text="pageTitle" />
+      <div class="w-1/2 mx-auto flex flex-col justify-center gap-10">
+        <div class="flex gap-2 mx-auto">
+          <input v-model="newTodo.title" type="text" ref="input" @keyup.enter="() => addTodo()" />
+          <button class="py-1 px-2 border-2 rounded-md" @click="() => addTodo()">Add</button>
+        </div>
+        <div class="flex flex-col gap-2 items-center">
+          <div class="flex flex-col gap-2 w-full">
+            <div
+              class="w-full flex gap-2 p-5 rounded-md bg-black"
+              v-for="(todo, index) in todoList"
+              :key="`todo-id-${todo.id}`"
+            >
+              <input type="checkbox" :checked="todo.checked" />
+              <span class="text-lg capitalize">{{ todo.title }}</span>
+              <button class="py-1 px-2 bg-red-500" @click="() => deleteTodo(index)">DELETE</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -25,10 +35,15 @@ const newTodo = ref({
   title: '',
   text: 'hello',
   importance: { color: 'green', lvl: 1 },
-  date: '',
+  date: {},
+  checked: false,
 });
 
-const deleteTodo = (id) => {};
+const deleteTodo = (todoIndex) => {
+  todoList.value.splice(todoIndex, 1);
+
+  saveTodo();
+};
 
 const addTodo = () => {
   newTodo.value.date = new Date();
