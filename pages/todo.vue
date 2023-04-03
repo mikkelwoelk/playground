9ef852a8884e6e903ec40da4a6f84dd71a3e7d6c
mikkelwoelk <mikkelwolk@gmail.com>
2023-04-03 09:13:40 +0200

    feat(todo): added more features

--- pages/todo.vue
+++ pages/todo.vue
@@ -3,12 +3,13 @@
     <BackButton />
     <h1 class="text-center text-6xl md:text-8xl mb-10 md:mb-20" v-text="pageTitle" />
     <div>
-      <button @click="() => fetchTodoList()">Fetch</button>
+      <input v-model="newTodo.title" type="text" ref="input" @keyup.enter="() => addTodo()" />
+      <button class="py-1 px-2 border-2" @click="() => addTodo()">Add</button>
       <ul>
         <li v-for="todo in todoList" :key="`todo-id-${todo.id}`">
-          <input type="checkbox" :checked="todo.completed" />
+          <!-- <input type="checkbox" /> -->
           {{ todo.title }}
-          <button @click="() => deleteTodo(todo.id)">DELETE</button>
+          <!-- <button class="py-1 px-2 bg-red-500" @click="() => deleteTodo(todo.id)">DELETE</button> -->
         </li>
       </ul>
     </div>
@@ -17,23 +18,53 @@
 
 <script setup>
 const pageTitle = ref('Todo list');
+const input = ref(null);
 const todoList = ref([]);
+const newTodo = ref({
+  id: '',
+  title: '',
+  text: 'hello',
+  importance: { color: 'green', lvl: 1 },
+  date: '',
+});
 
-const fetchTodoList = () => {
-  fetch('https://jsonplaceholder.typicode.com/todos/')
-    .then((response) => response.json())
-    .then((json) => {
-      todoList.value = json;
-    });
+const deleteTodo = (id) => {};
+
+const addTodo = () => {
+  newTodo.value.date = new Date();
+  newTodo.value.id = Math.random().toString(36).substring(2);
+
+  todoList.value.push(newTodo.value);
+
+  saveTodo();
+
+  newTodo.value = {
+    title: '',
+    description: '',
+    importance: {
+      color: newTodo.value.importance.color,
+      lvl: newTodo.value.importance.lvl,
+    },
+    date: {},
+  };
+
+  input.value.focus();
 };
 
-const deleteTodo = (id) => {
-  fetch(`https://jsonplaceholder.typicode.com/todos/1`, {
-    method: 'DELETE',
-  });
-
-  console.log(`"deleted" todo ${id}`);
+const saveTodo = () => {
+  const parsedTodo = JSON.stringify(todoList.value);
+  localStorage.setItem('todoList', parsedTodo);
 };
+
+onMounted(() => {
+  if (localStorage.getItem('todoList')) {
+    try {
+      todoList.value = JSON.parse(localStorage.getItem('todoList'));
+    } catch (e) {
+      localStorage.removeItem('todoList');
+    }
+  }
+});
 </script>
 
 <style></style>
