7e06447cd067a6c5f3c2f15872f5a35ae0d28ca0
mikkelwoelk <mikkelwolk@gmail.com>
2023-04-05 12:07:03 +0200

    fix(BackButton): changed hover animation

--- components/global/BackButton.vue
+++ components/global/BackButton.vue
@@ -1,13 +1,18 @@
 <template>
-  <button class="c-back-button absolute">
-    <NuxtLink :to="'/'">
+  <div class="c-back-button w-10 h-10 absolute border-[3px] rounded-full overflow-hidden">
+    <NuxtLink class="flex relative group" :to="'/'">
       <img
-        class="w-10 fill-white hover:-translate-x-1 transition-all duration-200 ease-in-out"
+        class="absolute w-10 fill-white group-hover:-translate-x-5 group-hover:opacity-0 transition-all duration-300 ease-in-out"
+        src="/arrow-left-icon.svg"
+        alt=""
+      />
+      <img
+        class="absolute w-10 fill-white translate-x-10 opacity-0 group-hover:-translate-x-0 group-hover:opacity-100 transition-all duration-300 ease-in-out"
         src="/arrow-left-icon.svg"
         alt=""
       />
     </NuxtLink>
-  </button>
+  </div>
 </template>
 
 <script setup></script>
