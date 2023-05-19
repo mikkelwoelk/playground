8d4db6b865ef73222ff5b9120e1f460cf3ded27b
mikkelwoelk <mikkelwolk@gmail.com>
2023-05-19 19:02:29 +0200

    feat: added page transition

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
-    <NuxtPage />
-  </div>
+  <NuxtPage />
 </template>
+
+<style lang="postcss">
+.page-enter-active,
+.page-leave-active {
+  transition: all 500ms;
+}
+.page-enter-from {
+  opacity: 0;
+  transform: translate(-15px, -15px);
+}
+
+.page-leave-to {
+  opacity: 0;
+  transform: translate(15px, 15px);
+}
+</style>
