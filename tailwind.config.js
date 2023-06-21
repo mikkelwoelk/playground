2628fd2e9ea293ea9dd4a2c263b9ec627142c3b4
mikkelwoelk <mikkelwolk@gmail.com>
2023-06-21 09:36:22 +0200

    feat: added colors for dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,vue}'],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
-        bg: '#1d3557',
+        background: '#EDF2F4',
         primary: '#a8dadc',
-        secondary: '#457b9d',
-        text: '#f1faee',
-        cta: '#e63946',
+        secondary: '#344955',
+        text: '#2B2D42',
+        cta: '#f9aa33',
+        'dark-background': '#232f34',
+        'dark-primary': '#a8dadc',
+        'dark-secondary': '#344955',
+        'dark-text': '#c4d1dd',
+        'dark-cta': '#f9aa33',
       },
     },
   },
