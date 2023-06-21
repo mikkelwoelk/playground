7af2528dc35ab4b379a4b01a6b7804c5ec88ec4b
mikkelwoelk <mikkelwolk@gmail.com>
2023-06-21 09:40:12 +0200

    feat: added page load overlay and dark mode logic

--- app.vue
+++ app.vue
@@ -1,19 +1,132 @@
 <template>
-  <NuxtPage />
+  <main id="main">
+    <NuxtPage />
+    <div class="fixed top-10 right-5 flex gap-5">
+      <button @click="changeTheme('dark')">dark</button>
+      <button @click="changeTheme('light')">light</button>
+      <button @click="changeTheme()">opp</button>
+    </div>
+    <div class="t-page-main__cover">
+      <div class="w-full h-full grid place-content-center">
+        <div class="w-full h-full">
+          <div class="t-page-main__cover-text flex w-full h-full">
+            <span>W</span>
+            <span>&lt;</span>
+            <span>/</span>
+            <span>></span>
+            <span>L</span>
+            <span>K</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
+<script setup>
+const changeTheme = (theme) => {
+  if (theme === 'dark') {
+    localStorage.theme = 'dark';
+  }
+  if (theme === 'light') {
+    localStorage.theme = 'light';
+  } else {
+    localStorage.removeItem('theme');
+  }
+  setTheme();
+};
+
+const setTheme = () => {
+  localStorage.theme === 'dark' ||
+  (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+    ? document.documentElement.classList.add('dark')
+    : document.documentElement.classList.remove('dark');
+};
+
+onMounted(() => {
+  setTheme();
+});
+</script>
+
 <style lang="postcss">
-.page-enter-active,
-.page-leave-active {
-  transition: all 500ms;
+.t-page-main-enter-active,
+.t-page-main-leave-active {
+  @apply duration-1000 ease-in-out;
 }
-.page-enter-from {
-  opacity: 0;
-  transform: translate(-15px, -15px);
+.t-page-main-enter {
+  @apply opacity-80 -translate-y-20;
+}
+.t-page-main-leave-to {
+  @apply opacity-80 translate-y-20;
 }
 
-.page-leave-to {
-  opacity: 0;
-  transform: translate(15px, 15px);
+.t-page-main__cover {
+  @apply fixed justify-center items-center left-0 top-0 w-full h-full;
+  display: none;
+  /* background: radial-gradient(circle, rgba(96, 142, 144, 1) 0%, rgba(53, 92, 116, 1) 50%, rgba(21, 39, 64, 1) 100%); */
+  background-color: rgb(224, 229, 235);
+  z-index: 1100;
+}
+.t-page-main__cover:first-child,
+.t-page-main-leave-active ~ .t-page-main__cover {
+  @apply flex;
+  animation: t-page-main__cover--part-1 500ms forwards;
+  animation-iteration-count: 1;
+}
+.t-page-main-enter-active ~ .t-page-main__cover {
+  @apply flex;
+  animation: t-page-main__cover--part-2 1000ms forwards;
+  animation-iteration-count: 1;
+}
+
+@keyframes t-page-main__cover--part-1 {
+  0% {
+    clip-path: inset(0 0 100% 0);
+  }
+  100% {
+    clip-path: inset(0 0 0 0);
+  }
+}
+@keyframes t-page-main__cover--part-2 {
+  0% {
+    clip-path: inset(0 0 0 0);
+  }
+  100% {
+    clip-path: inset(100% 0 0 0);
+  }
+}
+/* @keyframes t-page-main__cover--part-2 {
+  0% {
+    clip-path: circle(100%);
+  }
+  100% {
+    clip-path: circle(0);
+  }
+} */
+.t-page-main__cover-text span {
+  font-family: 'Poppins', sans-serif;
+  font-size: 15vw;
+  color: rgb(224, 229, 235);
+  text-shadow: -0.3rem -0.6rem 1rem rgba(242, 242, 242, 0.502), 0.3rem 0.6rem 1rem rgba(0, 0, 0, 0.05);
+  caret-color: #262626;
+  outline: none;
+}
+.t-page-main__cover-text {
+  gap: 2.5vw;
+}
+.t-page-main__cover:first-child .t-page-main__cover-text,
+.t-page-main-leave-active ~ .t-page-main__cover .t-page-main__cover-text {
+  animation: spread-text 1000ms ease-in-out forwards;
+  animation-iteration-count: 1;
+}
+
+@keyframes spread-text {
+  0% {
+    gap: 0;
+    opacity: 0;
+  }
+  100% {
+    gap: 2.5vw;
+  }
 }
 </style>
