67ad1ae5ecbd629dbfbf8d371aabcb1e61f8f9e4
mikkelwoelk <mikkelwolk@gmail.com>
2023-06-21 09:38:48 +0200

    fix: changed styling

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
       <div
         class="c-page-list grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 max-w-7xl mx-auto"
       >
-        <div class="c-page-list__item relative overflow-hidden shadow-2xl rounded-xl" v-for="(page, index) in pages">
+        <div class="c-page-list__item rounded-xl" v-for="(page, index) in pages">
           <NuxtLink :to="page.url">
             <CardContainer :card-content="pages[index]"></CardContainer>
           </NuxtLink>
@@ -39,7 +39,39 @@ const pages = ref([
   }
 }
 
-.c-page-list__item:hover:before {
+.c-page-list__item {
+  position: relative;
+  /* box-shadow: 3px 3px 5px #263540, -3px -3px 5px #3d5567; */
+  background-color: var(--color-secondary);
+  transition: all ease-in-out 100ms;
+}
+/* .c-page-list__item:before {
+  content: '';
+  position: absolute;
+  @apply w-full h-full -top-[4%] -left-[1.5%] bg-white rounded-xl blur-sm;
+  mix-blend-mode: saturation;
+  z-index: -1;
+} */
+.c-page-list__item .c-card-title {
+  @apply duration-200 ease-in-out;
+}
+.c-page-list__item:hover .c-card-title {
+  @apply text-cta;
+}
+.c-page-list__item .c-card-info {
+  @apply duration-200 ease-in-out;
+  opacity: 0.4;
+}
+.c-page-list__item:hover .c-card-info {
+  opacity: 1;
+}
+.c-page-list__item:active {
+  box-shadow: inset 3px 3px 5px #263540, inset -3px -3px 5px #3d5567;
+  transform: scale(0.99999);
+  background-color: #2e3f4d;
+}
+
+/* .c-page-list__item:hover:before {
   content: '';
   position: absolute;
   width: 100px;
@@ -54,5 +86,5 @@ const pages = ref([
   top: 0;
   left: -100px;
   animation: shine 1500ms forwards;
-}
+} */
 </style>
