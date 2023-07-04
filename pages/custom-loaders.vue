61377654b480554f5e6eacc08ce3efdd1d4ca814
mikkelwoelk <mikkelwolk@gmail.com>
2023-07-04 15:25:13 +0200

    feat: added dots loader

--- pages/custom-loaders.vue
+++ pages/custom-loaders.vue
@@ -20,8 +20,14 @@
         </div>
       </div>
       <div class="loader">
-        <div class="relative w-12 h-12">
-          <span class="loader-dots"></span>
+        <div class="w-20 h-12">
+          <div class="loader-dots">
+            <svg viewBox="0 0 80 48" xmlns="http://www.w3.org/2000/svg">
+              <circle class="dot" cx="0" cy="25" r="8"></circle>
+              <circle class="dot" cx="0" cy="25" r="8" style="animation-delay: 1000ms"></circle>
+              <circle class="dot" cx="0" cy="25" r="8" style="animation-delay: 2000ms"></circle>
+            </svg>
+          </div>
         </div>
       </div>
     </div>
@@ -37,8 +43,6 @@ const pageTitle = ref('custom loaders');
   @apply border-2 border-dark-secondary dark:border-secondary border-opacity-30 dark:border-opacity-30 w-full h-full p-5 grid place-content-center aspect-square rounded-lg;
 }
 
-/* .loader-circle {
-} */
 .loader-circle circle {
   @apply fill-transparent stroke-dark-secondary dark:stroke-secondary;
   stroke-dasharray: 135;
@@ -85,6 +89,37 @@ const pageTitle = ref('custom loaders');
   }
   100% {
     @apply w-[25%] h-[25%] bottom-0 right-0;
+  }
+}
+
+.loader-dots .dot {
+  @apply fill-dark-secondary dark:fill-secondary rounded-full opacity-0;
+  animation: loader-dot ease-in-out 3000ms infinite;
+}
+
+@keyframes loader-dot {
+  0% {
+    @apply opacity-0;
+    cx: 40;
+    r: 0;
+  }
+  20% {
+    cx: 16;
+    r: 5;
+  }
+  50% {
+    @apply opacity-100;
+    cx: 40;
+    r: 10;
+  }
+  80% {
+    cx: 64;
+    r: 5;
+  }
+  100% {
+    @apply opacity-0;
+    cx: 40;
+    r: 0;
   }
 }
 </style>
