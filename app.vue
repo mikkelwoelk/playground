7756a9c7b342b44057ea4ba6b2ee049d1d7173f5
mikkelwoelk <mikkelwolk@gmail.com>
2023-07-04 13:21:20 +0200

    feat: added default layout

--- app.vue
+++ app.vue
@@ -1,24 +1,25 @@
 <template>
-  <main id="main">
-    <SiteNavigation />
-    <NuxtPage />
-    <SiteFooter />
-
-    <div class="t-page-main__cover">
-      <div class="w-full h-full grid place-content-center">
-        <div class="w-full h-full">
-          <div class="t-page-main__cover-text flex w-full h-full">
-            <span>W</span>
-            <span>&lt;</span>
-            <span>/</span>
-            <span>></span>
-            <span>L</span>
-            <span>K</span>
+  <SiteNavigation />
+  <NuxtLayout>
+    <main id="main">
+      <NuxtPage />
+      <div class="t-page-main__cover">
+        <div class="w-full h-full grid place-content-center">
+          <div class="w-full h-full">
+            <div class="t-page-main__cover-text flex w-full h-full">
+              <span>W</span>
+              <span>&lt;</span>
+              <span>/</span>
+              <span>></span>
+              <span>L</span>
+              <span>K</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </main>
+    </main>
+  </NuxtLayout>
+  <SiteFooter />
 </template>
 
 <script setup></script>
@@ -36,8 +37,7 @@
 }
 
 .t-page-main__cover {
-  @apply hidden fixed justify-center items-center left-0 top-0 w-full h-full;
-  display: none;
+  @apply hidden fixed justify-center items-center left-0 top-0 w-full h-full bg-secondary dark:bg-dark-secondary;
   /* background: radial-gradient(circle, rgba(96, 142, 144, 1) 0%, rgba(53, 92, 116, 1) 50%, rgba(21, 39, 64, 1) 100%); */
   background-color: rgb(224, 229, 235);
   z-index: 1100;
@@ -70,22 +70,21 @@
     clip-path: inset(100% 0 0 0);
   }
 }
-/* @keyframes t-page-main__cover--part-2 {
-  0% {
-    clip-path: circle(100%);
-  }
-  100% {
-    clip-path: circle(0);
-  }
-} */
+
 .t-page-main__cover-text span {
-  font-family: 'Poppins', sans-serif;
+  /* font-family: 'Poppins', sans-serif; */
   font-size: 15vw;
   color: rgb(224, 229, 235);
   text-shadow: -0.3rem -0.6rem 1rem rgba(242, 242, 242, 0.502), 0.3rem 0.6rem 1rem rgba(0, 0, 0, 0.05);
-  caret-color: #262626;
   outline: none;
 }
+
+/* @media screen and (prefers-color-scheme: dark) {
+  .t-page-main__cover-text span {
+    color: #344955;
+    text-shadow: -0.3rem -0.6rem 1rem #374e5c, 0.3rem 0.6rem 1rem #2e414b;
+  }
+} */
 .t-page-main__cover-text {
   gap: 2.5vw;
 }
